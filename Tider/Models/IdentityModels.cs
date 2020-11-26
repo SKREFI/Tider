963445e2895c9595f83f2cad59633cd46226f7b1
SKREFI <srazvan43@yahoo.com>
2020-11-26 22:45:33 +0300

    first commit in the 2nd repo

---
+++ Tider/Models/IdentityModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Tider.Models {
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser {
+        public string Nickname { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Tider/Models/Models.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Tider.Models
+{
+    public class Category {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image_url { get; set; }
+        public DateTime Date { get; set; }
+        public string OpId { get; set; }           // Linked1
+        public virtual ApplicationUser Op { get; set; }     // Linked1
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+    public class Post {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Image_url { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ApplicationUser OP { get; set; }
+    }
+
+    
+
+
+    //public class Comment
+    //{
+    //    public int ID { get; set; }
+    //    public int Op_id { get; set; } // User connection
+    //    public string Content { get; set; }
+    //    public DateTime Date { get; set; }
+    //    public virtual Post Post { get; set; } // Posts ->>> Comments
+    //}
+
+    // ----------------------------------
+
+    //public class View
+    //{
+    //    public int ID { get; set; }
+    //    public int User_id { get; set; } // User connection, TODO: may be replaced with virtual User
+    //    public DateTime Date { get; set; }
+    //    //public int Post_id { get; set; } // Post connection
+    //    public virtual Post Post { get; set; }
+    //}
+
+    //public class Post
+    //{
+    //    public int ID { get; set; }
+    //    public int Op_id { get; set; } // User connection
+    //    public string Title { get; set; } 
+    //    public string Content { get; set; }
+    //    public string Image_url { get; set; }
+    //    public DateTime Date { get; set; }
+    //    public virtual Category Category { get; set; } // Category ->>> Post
+    //    public virtual ICollection<Comment> Comments { get; set; } // Comment -> Post
+    //    public virtual ICollection<View> Views { get; set; } // View ->>> Post
+    //}
+}
